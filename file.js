3ed5d4ebf4af572ec7c33a935be3197788907502
Yasuo67 <aomiii876@gmail.com>
2024-09-25 16:13:37 +0530

    implemented createGrids function to create grids depending on user input

--- file.js
+++ file.js
@@ -1,7 +1,25 @@
+createGrids(10);
+
 const slider = document.getElementById("grid-size-value");
 slider.addEventListener('input', setGridNumber);
 
 function setGridNumber(e){
     const gridNumber = e.target.value;
     document.getElementById("grid-number").textContent = gridNumber;
+    createGrids(gridNumber);
+}
+
+const drawingAreaSize = 640;
+
+function createGrids(gridNumber){
+    const drawingArea = document.getElementById("drawing-area");
+    drawingArea.innerHTML = '';
+    let gridBoxSize = 640/gridNumber;
+    for(let i=0; i< gridNumber*gridNumber; i++){
+            const gridBox = document.createElement("div");
+            gridBox.style.width = gridBoxSize+'px';
+            gridBox.style.height = gridBoxSize+'px';
+            gridBox.classList.add("box-style");
+            drawingArea.appendChild(gridBox);
+    }
 }
