b683e674aa6c7ea164a15fc70832fc442b94c501
Yasuo67 <aomiii876@gmail.com>
2024-09-25 17:40:19 +0530

    implemented functions for the boxes to change color on click and move, added a clear button

--- file.js
+++ file.js
@@ -1,5 +1,6 @@
 createGrids(10);
 
+
 const slider = document.getElementById("grid-size-value");
 slider.addEventListener('input', setGridNumber);
 
@@ -20,6 +21,36 @@ function createGrids(gridNumber){
             gridBox.style.width = gridBoxSize+'px';
             gridBox.style.height = gridBoxSize+'px';
             gridBox.classList.add("box-style");
+            // gridBox.addEventListener('click', draw);
             drawingArea.appendChild(gridBox);
     }
+}
+
+const colorPicker = document.getElementById("color-selected");
+let colorSelected = colorPicker.value;
+colorPicker.addEventListener('input', setColor);
+
+function setColor(e){
+    colorSelected = e.target.value;
+}
+
+let isMouseDown = false;
+const drawingArea = document.getElementById("drawing-area");
+drawingArea.addEventListener('mousedown', function(e){
+    isMouseDown = true;
+    e.target.style.backgroundColor = colorSelected;
+})
+drawingArea.addEventListener('mousemove', function(e){
+    if(isMouseDown == true){
+        e.target.style.backgroundColor = colorSelected;
+    }
+})
+window.addEventListener('mouseup', function () {
+    isMouseDown = false; 
+  });
+
+document.getElementById("clear-button").addEventListener("click", clearDrawingArea);
+function clearDrawingArea(){
+    const boxes = drawingArea.querySelectorAll(".box-style");
+    boxes.forEach(box => box.style.backgroundColor = "#ffffff");
 }
